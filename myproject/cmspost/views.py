8eaebf8e81a968d9ec622b77725b9484129e5ce0
Laura <laura.mo.go@hotmail.com>
2018-04-23 14:13:47 +0200

    Mejoras para /edit

--- myproject/cmspost/views.py
+++ myproject/cmspost/views.py
@@ -4,6 +4,15 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.template.loader import get_template
 from django.template import Context
 
+formulario = """
+
+<form action="" method="POST">
+ Página: <input type="text" name="pag"><br>
+ Url: <input type="text" name="url"><br>
+ <input type="submit" name="Enviar">
+</form>
+
+"""
 
 def autentificar(request):
 
@@ -53,6 +62,19 @@ def process_templates(request, rec):
             p = Context({'title': logged, 'contenido': respuesta})
             renderiz = plant.render(p)
             return HttpResponse(renderiz)
+        except Pages.DoesNotExist:
+            return HttpResponseNotFound(respuesta + "<br>La página /" + rec + "no ha sido encontrada en la base de datos")
+            
+def edit(request, rec):
+    
+    respuesta = ""
+    logged = autentificar(request)
+    
+    if request.method == "GET":
+        try:
+            lista = Pages.objects.get(name=rec)
+            respuesta += formulario
+            return HttpResponse(respuesta)       
         except Pages.DoesNotExist:
             return HttpResponseNotFound(respuesta + "<br>La página /" + rec + "no ha sido encontrada en la base de datos")
 
